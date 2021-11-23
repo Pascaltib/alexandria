7bf44dd7a6a5d14e5b3d4448b3ea9bf11e730525
rayvenrey <rayven.rey@gmail.com>
2021-11-23 18:00:49 +0100

    added destroy link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # create a students
   def create
     @batch = Batch.find(params[:batch_id])
     @user = User.new(student_params)
@@ -38,12 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
-# to remove student
-
+  # to remove student
   def destroy
-    @batch = Batch.find(params[:batch_id])
-    @students.destroy
-    # render ???????
+    # @batch = Batch.find(params[:batch_id])
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to batch_users_path(@batch)
   end
 
   private
