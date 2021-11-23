450fd68e2973c4e7ba039484802a63388d9bf984
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-23 16:54:32 +0100

    done with batches index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :batches, except: %i[destroy edit update] do
     resources :bookings, only: [:create]
-    resources :costs, only: %i[create show update]
+    resources :costs, only: %i[create show update destroy]
     resources :users, only: [:index]
   end
 
