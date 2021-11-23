a0d2eeae1ac0f839709a629ae00b3e31ef97f6a0
rayvenrey <rayven.rey@gmail.com>
2021-11-23 13:50:50 +0100

    users controller generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing database..."
+Booking.delete_all
+Cost.delete_all
+Batch.delete_all
+User.delete_all
+
+puts "Creating seeds...."
+
+User.create!(
+  {
+    email: "admin@gmail.com",
+    password: "password",
+    first_name: "Admin",
+    last_name: "Admin",
+    admin: true
+  }
+)
+
+20.times do
+  User.create!(
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: "password",
+      admin: false
+    }
+  )
+end
+
+puts "Database created!"
