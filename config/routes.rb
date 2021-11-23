a1629e1ba6b1203195774a18aa5a00bb259cd07e
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-23 16:59:07 +0100

    routes style

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :path_prefix => 'my'
-
   root to: 'pages#home'
   resources :batches, except: %i[destroy edit update] do
     resources :bookings, only: [:create]
     resources :costs, only: %i[create show update destroy]
-    resources :users, only: [:index, :update, :destroy, :edit]
+    resources :users, only: %i[index update destroy edit]
   end
-
 end
