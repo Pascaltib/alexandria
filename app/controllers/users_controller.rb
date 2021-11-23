8b20df2d9f282c790aab0a1e164f5c3e1985d62b
rayvenrey <rayven.rey@gmail.com>
2021-11-23 16:37:39 +0100

    form edit fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @batch = Batch.find(params[:batch_id])
+    @user = User.find(params[:id])
+
   end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to adminpanel_path
+    else
+      render 'edit'
+    end
+  end
+
+
 
   def destroy
     @batch = Batch.find(params[:batch_id])
