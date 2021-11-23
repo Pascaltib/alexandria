c4d26f4a8bae8d54522757676abc89f5e38b2745
Pascal Tiberghien <pascaltib@gmail.com>
2021-11-23 13:49:33 +0100

    batch template

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,2 +1,14 @@
 class BatchesController < ApplicationController
+  def show
+    @batch = Batch.find(params[:id])
+    @costs = @batch.costs
+    @students = []
+    User.where(admin: false).each do |student|
+      @batch.bookings.each do |booking|
+        if booking.user_id == student.id
+          @students << student
+        end
+      end
+    end
+  end
 end
