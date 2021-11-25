6c47f88c71016030536a6d734539be7c679125ba
miguel rodriguez <aphex95@gmail.com>
2021-11-25 15:47:02 +0100

    changes

--- app/javascript/components/modal_button.js
+++ app/javascript/components/modal_button.js
@@ -1,14 +1,17 @@
 const modalButtons = () => {
   var modal = document.querySelector('.modal');
 
-  var showModal = document.querySelector('.show-modal');
+  var showModal = document.querySelectorAll('.show-modal');
   var closeModal = document.querySelectorAll('.close-modal');
 
   if (showModal){
-    showModal.addEventListener('click', function () {
+    showModal.forEach(element => {
+      element.addEventListener('click', function () {
       modal.classList.toggle('hidden')
+      });
     });
   };
+
   if(closeModal){
     closeModal.forEach(close => {
       close.addEventListener('click', function () {
