5ac7efb22a95e0079fcb667f05625a0d067e128f
miguel rodriguez <aphex95@gmail.com>
2021-11-26 15:46:07 +0100

    stuff

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -4,11 +4,4 @@ class Batch < ApplicationRecord
   has_many :costs
   validates :name, presence: true
   validates :tuition_cost, presence: true
-
-  include PgSearch::Model
-  pg_search_scope :search_by,
-    against: [ :first_name, :last_name, :email],
-    using: {
-      tsearch: { prefix: true } # this will return some....
-    }
 end
