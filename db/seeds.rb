1425c7dd11595ea9898d0cfa0b5e4bceb9b6e8bb
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-01 16:34:54 +0100

    seeds for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,9 @@ test_user = User.create!(
   }
 )
 
-test_batch = Batch.create!(user: test_user, name: "Madrid", tuition_cost: 6500, start_date: "Sun, 3 Oct 2021", end_date: "Fri, 3 Dec 2021")
+test_batch = Batch.create!(user: test_user, name: "Madrid", tuition_cost: 6500, start_date: "Fri, 3 Dec 2021", end_date: "Sat, 15 Jan 2022")
+test_batch2 = Batch.create!(user: test_user, name: "Barcelona", tuition_cost: 7000, start_date: "Fri, 3 Dec 2021", end_date: "Sat, 15 Jan 2022")
+
 count = 10
 11.times do
   temp = URI.open("https://cdn.devdojo.com/images/june2021/avt-#{count}.jpg")
@@ -35,6 +37,29 @@ count = 10
   else
     Booking.create!(user: student, batch: test_batch, status: "Accepted")
   end
+  puts count
+  count += 1
+end
+
+count = 10
+11.times do
+  temp = URI.open("https://cdn.devdojo.com/images/june2021/avt-#{count}.jpg")
+  student = User.create!(
+    {
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: "password",
+      admin: false
+    }
+  )
+  student.photo.attach(io: temp, filename: "nes.jpg", content_type: "image/jpg")
+  if count < 14
+    Booking.create!(user: student, batch: test_batch2)
+  else
+    Booking.create!(user: student, batch: test_batch2, status: "Accepted")
+  end
+  puts count
   count += 1
 end
 
@@ -44,5 +69,12 @@ Cost.create!(name: "Food", amount: 200, kind: "Variable", batch: test_batch, rec
 Cost.create!(name: "Drinks", amount: 100, kind: "Variable", batch: test_batch, recurring: "One Time")
 Cost.create!(name: "Chairs", amount: 200, kind: "Variable", batch: test_batch, recurring: "One Time")
 Cost.create!(name: "Tables", amount: 250, kind: "Variable", batch: test_batch, recurring: "One Time")
+
+Cost.create!(name: "Rent", amount: 15_000, kind: "Fixed", batch: test_batch2, recurring: "Monthly")
+Cost.create!(name: "Teacher Salary", amount: 300, kind: "Fixed", batch: test_batch2, recurring: "Daily")
+Cost.create!(name: "Food", amount: 200, kind: "Variable", batch: test_batch2, recurring: "One Time")
+Cost.create!(name: "Drinks", amount: 100, kind: "Variable", batch: test_batch2, recurring: "One Time")
+Cost.create!(name: "Chairs", amount: 200, kind: "Variable", batch: test_batch2, recurring: "One Time")
+Cost.create!(name: "Tables", amount: 250, kind: "Variable", batch: test_batch2, recurring: "One Time")
 
 puts "Database created!"
