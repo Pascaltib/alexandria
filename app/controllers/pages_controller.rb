6add79654f4294f594d03335d22d74c68a517e51
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-02 12:38:19 +0100

    log in problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if current_user.admin == false
+    if user_signed_in? && current_user.admin == false
       redirect_to batches_path
     end
   end
