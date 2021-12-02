248083de6dcccfdccbd8fcf8f50c011af4caf5a7
Pascal Tiberghien <pascaltib@gmail.com>
2021-12-02 12:14:48 +0100

    homepage redirect for students

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user.admin == false
+      redirect_to batches_path
+    end
   end
 end
